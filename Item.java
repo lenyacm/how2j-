4407000ddb75331f2555f344457e23638e3b2958
leny <1738516601@qq.com>
2020-11-06 21:07:28 +0800

    Item

---
+++ Item.java
@@ -0,0 +1,19 @@
+package how2j;
+
+public class Item {
+	String name;
+	int price;
+	public static void main(String[] args) {
+		Item xueping = new Item();
+		xueping.name = "ÑªÆ¿";
+		xueping.price = 50;
+		
+		Item caoxie = new Item();
+		caoxie.name = "²ÝÐ¬";
+		caoxie.price = 300;
+		
+		Item changjian = new Item();
+		changjian.name = "³¤½£";
+		changjian.price = 350;
+	}
+}
