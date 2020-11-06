8aa8ffca593e341a20af2f3f6489a94fd0c687eb
leny <1738516601@qq.com>
2020-11-06 22:41:05 +0800

    变量final

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,12 @@
 package how2j;
 
 public class HelloWorld {
-    int i = 1; //属性名是i
-    public void method1(int i){ //参数也是i
-        System.out.println(i);
+	 
+    public void method1(final int j) {
+        //j = 5; //cannot be assigned
     }
-     
+    
     public static void main(String[] args) {
-        new HelloWorld().method1(5);
-        //结果打印出来是 1还是5?
+    	new HelloWorld().method1(4);
     }
 }
