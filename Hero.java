0a4767a6f7fc8c19ad55286f7f6c603c505abcd3
leny <1738516601@qq.com>
2020-11-06 21:15:21 +0800

    属性

---
+++ Hero.java
@@ -0,0 +1,18 @@
+package how2j;
+
+public class Hero {
+	String name;
+	float hp;
+	float armor;
+	int moveSpeed;
+	
+	int killed;
+	int kill;
+	int support;
+	int money;
+	int lastHit;
+	int attackSpeed;
+	String wordAfterkill;
+	String wordAfterkiled;
+	
+}
